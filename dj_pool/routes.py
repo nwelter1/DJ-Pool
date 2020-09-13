3199d3a32a29afbd1af8c907761a36e0ef96c224
nwelter1 <nwelter1@gmail.com>
2020-09-13 15:35:12 -0500

    registration form page

--- dj_pool/routes.py
+++ dj_pool/routes.py
@@ -1,7 +1,14 @@
 from dj_pool import app
 from flask import render_template
+from dj_pool.forms import UserInfoForm
 
 # Home Route
 @app.route('/')
 def home():
     return render_template("home.html")
+
+# Register Route
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = UserInfoForm()
+    return render_template('register.html', form=form)
