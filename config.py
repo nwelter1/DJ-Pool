0862985f42331f811216a1228b4882d888c68725
nwelter1 <nwelter1@gmail.com>
2020-09-15 17:34:45 -0500

    adjusted mail server

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config():
     SECRET_KEY = os.environ.get('SECRET_KEY') or "secret key required..."
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir,'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = "smtp.sendgrid.com"
+    MAIL_SERVER = "smtp.sendgrid.net"
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = 'apikey'
